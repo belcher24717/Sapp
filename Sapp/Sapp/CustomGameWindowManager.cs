b6cd04e4b550dacd32385b23de1a90e36f23a458
ryanyoung1989 <ryanyoung1989@hotmail.com>
2015-05-13 18:24:55 -0700

    Added beginnings of Custom Game Wizard
    - Added a new window called CustomGameWizard
    - Added a new class CustomGameWindowManager
    - Added basic functionality (not complete) to CustomGameWindowManager
    class.
    - Added basic GUI wireframe (not complete) to CustomGameWizard Window.

---
+++ Sapp/Sapp/CustomGameWindowManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+
+namespace Sapp
+{
+    public class CustomGameWindowManager
+    {
+        // not sure if i need to keep track of currentTab at this point
+        private TabItem currentTab;
+        private TabControl wizardTab;
+
+        public CustomGameWindowManager(TabControl wizardTab, TabItem startingTab)
+        {
+            this.wizardTab = wizardTab;
+            this.currentTab = startingTab;
+            this.wizardTab.SelectedItem = this.currentTab;
+        }
+
+        public void SwitchTab(TabItem nextTab)
+        {
+            this.currentTab = nextTab;
+            this.wizardTab.SelectedItem = this.currentTab;
+        }
+
+        // not sure if i need this functionality at this point
+        public TabItem GetCurrentTab()
+        {
+            return this.currentTab;
+        }
+
+    }
+}
---
+++ Sapp/Sapp/CustomGameWizard.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sapp
+{
+    /// <summary>
+    /// Interaction logic for CustomGameWizard.xaml
+    /// </summary>
+    public partial class CustomGameWizard : Page
+    {
+        public CustomGameWizard()
+        {
+            InitializeComponent();
+
+            tabcontrol_mainview.SelectionChanged += ChangeButtonText;
+        }
+
+        private void ChangeButtonText(object sender, RoutedEventArgs e)
+        {
+            if (tabcontrol_mainview.SelectedIndex == 2)
+                button_next.Content = "Finish";
+            else
+                button_next.Content = "Next >";
+        }
+    }
+}
